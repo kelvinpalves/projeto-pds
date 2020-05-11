1fa472cc243358022aa33da90b248eba20629e39
Kelvin Alves <kalves@kopp.com.br>
2020-05-11 17:37:53 -0300

    First commit

---
+++ backend/src/main/java/br/unisc/gestaofrota/GestaofrotaApplication.java
@@ -0,0 +1,13 @@
+package br.unisc.gestaofrota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaofrotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaofrotaApplication.class, args);
+	}
+
+}
