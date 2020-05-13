021a61ac26a498f7c2550ce8d52a4ef6e847ddae
Kelvin Alves <kalves@kopp.com.br>
2020-05-13 19:26:00 -0300

    start front end - main/sidebar/navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './main/components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './main/components/side-bar/side-bar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoadingBarHttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true
+    }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
