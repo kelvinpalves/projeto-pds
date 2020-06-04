b51b534f86ca967db51b72829b03c8e4882b040f
Kelvin Alves <kalves@kopp.com.br>
2020-06-03 21:18:02 -0300

    adicionar ponto na rota

---
+++ backend/src/main/java/br/unisc/gestaofrota/api/rota/RotaService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unisc.gestaofrota.api.rota;
+
+/**
+ *
+ * @author kelvin
+ */
+public interface RotaService {
+    
+    RotaDto salvar(RotaDto dto) throws Exception;
+    
+}
