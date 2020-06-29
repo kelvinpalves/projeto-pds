3a5d0fcb334b3be2027dcecd401184afcc6d5a7c
joaogularte <jov96@hotmail.com>
2020-06-29 13:09:28 -0300

    Add maps markers

--- frontend/src/app/veiculo/components/veiculo-map/veiculo-map.component.ts
+++ frontend/src/app/veiculo/components/veiculo-map/veiculo-map.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./veiculo-map.component.css']
 })
 export class VeiculoMapComponent implements OnInit {
+  
+  latitudeScs: number = -29.717119;
+  longitudeScs: number = -52.428162;
+  zoom: number = 14
+  coordenadas: Array<Object>;
 
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit(): void {
+    this.coordenadas = [{
+      latitude: -29.713115,
+      longitude: -52.437536
+    }, {
+      latitude: -29.711251,
+      longitude: -52.429801
+    }, {
+      latitude: -29.745130,
+      longitude: -52.408733
+    }]
   }
 
 }
